cdb0b2cdc82106acefa73a120f852e611a5cb292
AndrieFranza <andrie.franza@cit.edu>
2025-05-08 21:35:53 +0800

    Add files via upload

---
+++ form.java
@@ -0,0 +1,119 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+public class form extends JFrame {
+    private JPanel pnlMain;
+    private JTextField textField1;
+    private JButton button1;
+    private JTextArea textArea1;
+    private JRadioButton rbRock;
+    private JRadioButton rbPaper;
+    private JRadioButton rbScissors;
+
+    public form() {
+        setTitle("Rock Paper Scissors");
+
+        ButtonGroup group = new ButtonGroup();
+        group.add(rbRock);
+        group.add(rbPaper);
+        group.add(rbScissors);
+
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                playGame();
+            }
+        });
+    }
+
+    public void playGame() {
+        String name = textField1.getText().trim();
+        if (name.isEmpty()) {
+            JOptionPane.showMessageDialog(this, "Please enter your name.");
+            return;
+        }
+
+        String move = null;
+        if (rbRock.isSelected()) move = "Rock";
+        else if (rbPaper.isSelected()) move = "Paper";
+        else if (rbScissors.isSelected()) move = "Scissors";
+
+        if (move == null) {
+            JOptionPane.showMessageDialog(this, "Please select a move.");
+            return;
+        }
+
+        Player player = new Player(name);
+        player.setMove(move);
+
+        Game game = new GameInfo(); // Polymorphism
+        String result = game.play(player.getMove());
+
+        textArea1.setText("Player: " + player.getName() + "\n" + result);
+    }
+
+    public static void main(String[] args) {
+        form game = new form();
+        game.setContentPane(game.pnlMain);
+        game.setSize(400, 300);
+        game.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        game.setVisible(true);
+    }
+
+    public abstract static class Game {
+
+        public abstract String play(String playerMove);
+
+    }
+
+    public static class GameInfo extends Game {
+
+            private final String[] moves = {"Rock", "Paper", "Scissors"};
+            private final Random rand = new Random();
+
+            @Override
+            public String play(String playerMove) {
+                String computerMove = moves[rand.nextInt(moves.length)];
+                String result;
+
+                if (playerMove.equals(computerMove)) {
+                    result = "It's a draw!";
+                } else if (
+                        (playerMove.equals("Rock") && computerMove.equals("Scissors")) ||
+                                (playerMove.equals("Paper") && computerMove.equals("Rock")) ||
+                                (playerMove.equals("Scissors") && computerMove.equals("Paper"))
+                ) {
+                    result = "You win!";
+                } else {
+                    result = "Computer wins!";
+                }
+
+                return "Your Move: " + playerMove + "\nComputer Move: " + computerMove + "\nResult: " + result;
+            }
+        }
+
+    public static class Player {
+
+            private final String name;
+            private String move;
+
+            public Player(String name) {
+                this.name = name;
+            }
+
+            public String getName() {
+                return name;
+            }
+
+            public void setMove(String move) {
+                this.move = move;
+            }
+
+            public String getMove() {
+                return move;
+            }
+        }
+}
+
