b35b6c2609ae60ea62f0eaa34b253ef51a497665
AndrieFranza <andrie.franza@cit.edu>
2025-05-08 21:36:26 +0800

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,21 @@
+public class Player {
+
+    private String name;
+    private String move;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setMove(String move) {
+        this.move = move;
+    }
+
+    public String getMove() {
+        return move;
+    }
+}
